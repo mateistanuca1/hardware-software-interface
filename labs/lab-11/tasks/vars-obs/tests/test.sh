5c8dc6a04e0950785ecc9da2b314e955a0c78fb6
mateistanuca1 <unanumearmando@gmail.com>
2025-03-02 16:09:32 +0200

    Added all checkers
    Signed-off-by: mateistanuca1 <unanumearmando@gmail.com>

---
+++ labs/lab-11/tasks/vars-obs/tests/test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-3-Clause
+
+source graded_test.inc.sh
+
+
+if test -z "$SRC_PATH"; then
+    SRC_PATH=./../support
+fi
+
+if test -z "$TEST_PATH"; then
+    TEST_PATH="${PWD}"
+fi
+
+
+test_vars-obs()
+{
+    cd "$SRC_PATH"
+    make clean > /dev/null 2>&1
+    make > /dev/null 2>&1
+    ./main > main.out 2>&1
+    if grep -q "10" main.out && grep -q "33" main.out; then
+        rm main.out
+        make clean > /dev/null 2>&1
+        exit 0
+    else
+        make clean > /dev/null 2>&1
+        rm main.out
+        exit 1
+    fi
+}
+
+
+run_test test_vars-obs 100
